d549f63481b2b43d4eb26757abecaf907b5edb63
Gabriel <gabrielvipeixoto7@gmail.com>
2025-10-06 13:32:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import vercel from "vite-plugin-vercel"; // <-- Adicione esta linha
 
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), vercel(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
