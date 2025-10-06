1270631cab341baf77eeaf118e5581581321ed42
Gabriel <gabrielvipeixoto7@gmail.com>
2025-10-06 13:26:13 -0300

    teste 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import vercel from "vite-plugin-vercel"; // 1. Importe o plugin
+import vercel from "vite-plugin-vercel"; // <-- Adicione esta linha
 
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  // 2. Adicione o vercel() na lista de plugins
   plugins: [react(), vercel(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
